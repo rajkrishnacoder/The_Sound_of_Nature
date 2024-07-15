57d241e1dd9d3503f5c8c5142e258387b97cfdbb
Rajkrishna debnath <rajkrishnadebnath53@gmail.com>
2024-07-15 17:41:26 +0530

    testing new features for my project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <div className='h-screen gap-1 bg-slate-900 grid place-content-center'>
+      <div className="relative">
+        <div className="absolute -inset-0.5 opacity-75 bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg blur"></div>
+      <button className="px-7  relative bg-black rounded-lg loading-none flex items-center py-2 divide-x divide-gray-600">
+        <span className="text-gray-100 pr-6">Labs realease</span>
+        <span className="text-indigo-400 pl-6">SUBMIT</span>
+      </button>
+      </div>
+    </div>
   )
 }
 
