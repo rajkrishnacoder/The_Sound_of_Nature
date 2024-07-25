6d00492b19ca6f2f269648bc94b08c76e8f63233
Rajkrishna debnath <rajkrishnadebnath53@gmail.com>
2024-07-25 21:07:07 +0530

    adding sounds for buttons

--- src/component/cards/PlayCard.jsx
+++ src/component/cards/PlayCard.jsx
@@ -12,13 +12,13 @@ function PlayCard() {
         <div className='p-4 pb-6  w-full gap-[0.6rem] flex items-center justify-center  flex-col bg-black'>
 
             <div className='flex items-center gap-3 h-[2.6rem]' >
-                <Icon icon="material-symbols:skip-next-rounded" width="2.4rem" height="2.4rem" flip='horizontal' style={{color: "white"}} />
+                <Icon icon="material-symbols:skip-next-rounded" width="2.4rem" height="2.4rem" flip='horizontal' style={{color: "white"}} className='cursor-pointer' />
                
                 <div onClick={play} className=' h-[2.2rem] w-[2.2rem] bg-white flex items-center justify-center rounded-full cursor-pointer'>
                    <Icon icon={on ? "bi:pause-fill" : "pepicons-pop:play"} width={on ? "1.7rem" : "1.6rem"} height={on ? "1.7rem" : "1.6rem"}  style={{color: "black"}} />
                 </div>
 
-                 <Icon icon="material-symbols:skip-next-rounded" width="2.4rem" height="2.4rem"  style={{color: "white"}} />
+                 <Icon icon="material-symbols:skip-next-rounded" width="2.4rem" height="2.4rem"  style={{color: "white"}} className='cursor-pointer' />
             </div>
 
             <input type="range" max={100} min={0} className='h-1 cursor-pointer w-[20rem]' />
--- src/sounds/songs.js
+++ src/sounds/songs.js
@@ -1,47 +1,47 @@
 const songData = {
     "rain": {
         title: "rain",
-        audio : new Audio("https://res.cloudinary.com/dfnykewn7/video/upload/v1721662352/itgtms6buwr39ywlqiua.mp3"),
+        audio : new Audio("https://res.cloudinary.com/dfnykewn7/video/upload/v1721921020/rzd9zcjmf7ekrzbw3bne.mp3"),
         image: "arcticons:emoji-cloud-with-rain"
     },
     "light-rain": {
         title: "light-rain",
-        audio: new Audio(""),
+        audio: new Audio("https://res.cloudinary.com/dfnykewn7/video/upload/v1721921240/hfu40zwuiufbsy5toe8z.mp3"),
         image: "arcticons:rain"
     },
     "heavy-rain": {
         title: "heavy-rain",
-        audio: new Audio(""),
+        audio: new Audio("https://res.cloudinary.com/dfnykewn7/video/upload/v1721921177/xc5dlih3e0knftvvuvfp.mp3"),
         image: "bi:cloud-rain-heavy"
     },
     "wind": {
         title: "wind",
-        audio: new Audio(""),
+        audio: new Audio("https://res.cloudinary.com/dfnykewn7/video/upload/v1721920902/hk26qf2sgf6tf5jgkfbs.mp3"),
         image: "ph:wind-thin"
     }, 
     "thunder-distant": {
         title: "thunder-distant",
-        audio: new Audio(""),
+        audio: new Audio("https://res.cloudinary.com/dfnykewn7/video/upload/v1721921217/khqyaz7pf75xegrwksbq.mp3"),
         image: "arcticons:emoji-thunder-cloud-and-rain"
     }, 
     "thunder": {
         title: "thunder",
-        audio: new Audio(""),
+        audio: new Audio("https://res.cloudinary.com/dfnykewn7/video/upload/v1721921199/mik63qauxliimquulm4j.mp3"),
         image: "material-symbols-light:thunderstorm-outline"
     },
     "fire": {
         title: "fire",
-        audio: new Audio(""),
+        audio: new Audio("https://res.cloudinary.com/dfnykewn7/video/upload/v1721920957/nnfcxpr6t8gq9dlxtr5g.mp3"),
         image: "game-icons:campfire"
     },
     "wave": {
         title: "wave",
-        audio: new Audio(""),
+        audio: new Audio("https://res.cloudinary.com/dfnykewn7/video/upload/v1721921046/hlehxnylfkr1t9abdrpw.mp3"),
         image: "game-icons:wave-crest"
     },
     "cricket": {
         title: "cricket",
-        audio: new Audio(""),
+        audio: new Audio("https://res.cloudinary.com/dfnykewn7/video/upload/v1721921072/yx9ubnn2up9jkgirxgov.mp3"),
         image: "arcticons:emoji-cricket"
     }
 
